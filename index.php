e23c4c6c09e378d6f12594b45bf95904e5967520
debidattasuryaprakash <suryaprakash.d20@gmail.com>
2022-02-04 23:21:16 +0530

    Backend

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("includes/header.php");
-
-$preview = new PreviewProvider($con, $userLoggedIn);
-echo $preview->createPreviewVideo(null);
-
-$containers = new CategoryContainers($con, $userLoggedIn);
-echo $containers->showAllCategories();
-?>
